5cdc6738f57fdd7eb25da70b7ed0c13cebf0a1bf
Diogo-Galllina <diogogallina20@gmail.com>
2023-05-21 05:18:34 -0300

    Classes da pratica 2 criadas

--- 10-Polimorfismo-Pratica2/src/br/com/empresa/cadastro/entidades/Cliente.java
+++ 10-Polimorfismo-Pratica2/src/br/com/empresa/cadastro/entidades/Cliente.java
@@ -1,5 +1,25 @@
 package br.com.empresa.cadastro.entidades;
 
-public class Cliente {
+public class Cliente extends Pessoa{
+	private String titulo;
 
+	public Cliente(String nome, String titulo) {
+		super(nome);
+		this.titulo = titulo;
+	}
+	
+
+	public String exibirNomeFormatado() {
+		return titulo.toUpperCase() + " - " + super.getNome().toUpperCase();
+	}
+
+
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
 }
